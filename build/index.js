dcc2da2b7bf1d150d1dd314386e43e3680b191f2
Wambui_Maina <you@example.com>
2022-10-13 15:47:26 +0300

    js upto alert

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 //all the dimensions have been defined
 
-let fortyByForty,
+let boxesUsed
     thirtyByThirty,
     fiftyByFifty,
     sixtyBySixty,
@@ -10,4 +10,159 @@ let fortyByForty,
     w,
     l,
     area
-    
+
+let userForm,
+  userData = [];
+// Function to find number of boxes based on area
+function noOfBoxes() {
+  if (document.getElementById("floor").value == "30 by 30") {
+    boxesUsed= Math.floor(area()/1.53);
+  } else if (document.getElementById("floor").value == "40 by 40") {
+    boxesUsed = Math.floor(area()/1.92);}
+    else if (document.getElementById("floor").value == "50 by 50") {
+        boxesUsed = Math.floor(area()/1.75);}
+
+    else if (document.getElementById("floor").value == "60 by 60") {
+        boxesUsed = Math.floor(area()/1.44);
+  } 
+  else if (document.getElementById("floor").value == "60 by 30") {
+    boxesUsed = Math.floor(area()/1.44);}
+    else if (document.getElementById("wall").value == "25 by 40") {
+        boxesUsed = Math.floor(area()/1.5);}
+
+  else {
+    boxesUsed=Math.floor(area()/1.5)
+  return boxesUsed;
+}}
+
+// Function to find  Price of complementaries
+function compPrice () {
+  if (document.getElementById("comp").value == "grout") {
+    priceComp = 100;
+  } else if (document.getElementById("comp").value == "tileadh") {
+    priceComp = 650;
+  } else if (document.getElementById("comp").value == "Cornerstrips") {
+    priceComp = 100;
+  }  else {
+    priceComp= 200;
+  }
+  return priceComp;
+}
+
+// Function to find Pizza Price based on Toppings
+function tilePrice() {
+    noOfBoxes()
+  if (document.getElementById("floor").value == "30by30") {
+    priceOfTile = 820*boxesUsed;
+  } else if (document.getElementById("floor").value == "40 by 40") {
+    priceOfTile = 1200*boxesUsed;
+  } 
+  else if (document.getElementById("floor").value == "50 by 50") {
+    priceOfTile = 1800*boxesUsed;
+  }
+  else if (document.getElementById("floor").value == "60 by 60") {
+    priceOfTile = 2000*boxesUsed;
+  }
+  else if (document.getElementById("floor").value == "60 by 30") {
+    priceOfTile = 1200*boxesUsed;
+  }
+  else if (document.getElementById("wall").value == "40 by 25") {
+    priceOfTile = 800*boxesUsed;
+  }
+  else {
+    priceOfTile= 810*boxesUsed
+  }
+  return priceOfTile
+  
+}
+
+// Fuction to find Pizza Delivery Price
+function deliveryPrice() {
+  if (document.getElementById("del").value == "kajiado") {
+    priceDelivery = 0;
+  } else if (document.getElementById("del").value == "bluesky") {
+    priceDelivery = 1000;
+  } else if (document.getElementById("del").value == "Shapashina") {
+    priceDelivery = 1000;
+  } else if (document.getElementById("del").value == "Ilbissil") {
+    priceDelivery = 3000;
+  } else if (document.getElementById("del").value == "Namanga") {
+    priceDelivery = 7000;
+  } else if ((document.getElementById("del").value = "Isinya")) {
+    priceDelivery = 2000;
+  } else if (document.getElementById("del").value == "Mashuru") {
+    priceDelivery = 3000;
+  } else {
+    priceDelivery = 1500;
+  }
+  return priceDelivery;
+}
+
+//  Function to calculate the order price
+function orderPrice() {
+  noOfBoxes();
+  compPrice();
+  tilePrice();
+  orderCost = compPrice + tilePrice
+  userForm = {
+    floor: document.getElementById("floor").value,
+    comp: document.getElementById("comp").value,
+    wall: document.getElementById("wall").value
+  };
+  userData = Object.values(userForm);
+  alert(
+    "You have picked a " +
+      userForm.floor +
+      " " +
+      userForm.comp +
+      " " +
+      userForm.wall +
+      " Hi, The total charge for your tiles is " +
+      orderCost
+  );
+  cumulativePrice += orderCost;
+  alert("The total cost for Pizza(s) order is Kshs. " + cumulativePrice);
+  console.log(cumulativePrice);
+  let priceConfirm = confirm("Do you want to make another order?");
+  document.getElementById("form").reset();
+  if (priceConfirm == true) {
+    document.getElementById("form").reset();
+  } else {
+    orderConfirm = confirm("Do you want your Order Delivered?");
+    if (orderConfirm == true) {
+      // jQuery to display Delivery option
+      $(document).ready(function () {
+        $("#form-s").hide(function () {
+          $("#form1-h").show();
+        });
+      });
+    } else {
+      document.getElementById("form").reset();
+      history.go();
+      alert("Your order is ready for pick up from our location");
+    }
+  }
+}
+
+// Function to calculate the total price
+function totalOrderPrice() {
+  deliveryPrice();
+
+  priceTotal = cumulativePrice + priceDelivery; //Calculate order cost plus delivery
+  let finalOrder = " ";
+  userData.forEach(orderDetails);
+  function orderDetails(value, index, array){
+    finalOrder += value + " "
+    console.log(finalOrder)
+  }
+  alert(
+    "The total charge for your order Pizza(s) " +
+      finalOrder +
+      " and its delivery cost is " +
+      priceTotal
+  );
+
+  document.getElementById("form").reset();
+  history.go();
+  alert("Your order will be delivered at your location");
+}
