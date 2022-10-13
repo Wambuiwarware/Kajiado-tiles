2edce78849587a6cfe6c09ac8a0625761bdc317e
Wambui_Maina <you@example.com>
2022-10-13 14:07:47 +0300

    removed

---
+++ build/index.js
@@ -0,0 +1,13 @@
+//all the dimensions have been defined
+
+let fortyByForty,
+    thirtyByThirty,
+    fiftyByFifty,
+    sixtyBySixty,
+    sixtyByThirty,
+    fortyByTwentyFive,
+    thityByTwenty,
+    w,
+    l,
+    area
+    
